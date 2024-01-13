c863a7e3ee6877e262fa4318e0953a7350252721
KyleRobison15 <kylerobison15@gmail.com>
2024-01-12 17:23:13 -0700

    Create password input

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Box,
   Button,
@@ -6,10 +7,14 @@ import {
   FormControl,
   FormLabel,
   Heading,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
   VStack,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { BiHide, BiShow } from "react-icons/bi";
 import SplashBgLight from "../assets/Splash_Bg_Light.jpg";
 import SplashBgDark from "../assets/Splash_Bg_Dark.jpeg";
 import { focusBorderColors, formContainerBgColors } from "../utility/bgColors";
@@ -26,6 +31,10 @@ const Splash = () => {
     focusBorderColors.dark
   );
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => setShowPassword(!showPassword);
+
   return (
     <Flex
       h="100vh"
@@ -37,13 +46,16 @@ const Splash = () => {
       bgColor="rgba(255, 255, 255, 0.6)"
       justifyContent="center"
       alignItems="center"
+      pos="relative"
     >
       <Box
         p={10}
         borderRadius="10px"
         bgColor={formContainerBg}
-        w={[300, 420]}
+        w={[320, 420]}
         shadow="md"
+        pos="absolute"
+        top="25%"
       >
         <Heading textAlign="center" mb={4}>
           Climb on!
@@ -54,20 +66,46 @@ const Splash = () => {
           spacing={4}
           align="stretch"
         >
-          <FormControl>
-            <FormLabel>Username</FormLabel>
-            <Input focusBorderColor={inputFocusColor} type="text" />
+          <FormControl colorScheme="yellow">
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              id="username"
+              focusBorderColor={inputFocusColor}
+              type="text"
+            />
           </FormControl>
           <FormControl>
             <FormLabel>Password</FormLabel>
-            <Input focusBorderColor={inputFocusColor} type="password" />
+            <InputGroup>
+              <Input
+                focusBorderColor={inputFocusColor}
+                type={showPassword ? "text" : "password"}
+              />
+              <InputRightElement>
+                <IconButton
+                  aria-label={showPassword ? "Hide Password" : "Show Password"}
+                  icon={
+                    showPassword ? (
+                      <BiShow fontSize="20px" />
+                    ) : (
+                      <BiHide fontSize="20px" />
+                    )
+                  }
+                  onClick={handleShowPassword}
+                  isRound={true}
+                  colorScheme="yellow"
+                  variant="ghost"
+                  size="sm"
+                />
+              </InputRightElement>
+            </InputGroup>
           </FormControl>
           <Button type="submit" colorScheme="yellow" mt={4}>
             Sign in
           </Button>
           <Divider my={2} />
           <Flex gap={2} justifyContent="center">
-            <Box>
+            <Box textAlign="center">
               Dont have an account?
               <Button pl={2} colorScheme="yellow" variant="link">
                 Sign up
