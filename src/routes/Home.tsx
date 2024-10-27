f43ae3b55a1cd45db6fdd02e4cd2ea473276a089
KyleRobison15 <kylerobison15@gmail.com>
2024-10-27 12:06:21 -0600

    Stub out Home layout

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show, Stack } from "@chakra-ui/react";
+import { Flex, Grid, GridItem, Show, Stack } from "@chakra-ui/react";
 import useAuth from "../hooks/useAuth";
 import useLoading from "../hooks/useLoading";
 
@@ -9,24 +9,30 @@ const Home = () => {
   return isLoading ? (
     <div>Loading.....</div>
   ) : (
-    <Grid
-      templateAreas={{
-        base: `"main"`,
-        lg: `"aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
-      }}
-      p="10px 30px"
-    >
-      <Show above="lg">
-        <GridItem area="aside" bg="gray.50"></GridItem>
-      </Show>
-      <GridItem area="main" bg="gray.100" w="100%">
+    // <Grid
+    //   templateAreas={{
+    //     base: `"main"`,
+    //     lg: `"aside main"`,
+    //   }}
+    //   templateColumns={{
+    //     base: "1fr",
+    //     lg: "200px 1fr",
+    //   }}
+    //   p="10px 30px"
+    // >
+    //   <Show above="lg">
+    //     <GridItem area="aside" bg="gray.50"></GridItem>
+    //   </Show>
+    //   <GridItem area="main" bg="gray.100" w="100%">
+    //     Test
+    //   </GridItem>
+    // </Grid>
+    <Flex id="home-container" p="10px 30px" w="100%">
+      <Flex id="main-container" w="300px" bg="white">
         Test
-      </GridItem>
-    </Grid>
+      </Flex>
+      <Flex id="aside-container" flexGrow={1} bg="gray.50">Test</Flex>
+    </Flex>
   );
 };
 
